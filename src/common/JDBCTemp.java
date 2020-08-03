00d839c4f997803dbd671da628bc2d7448add618
suugit <69067257+suugit@users.noreply.github.com>
2020-08-03 18:12:36 +0900

    JDBCTemp 수정
    수정해봤어요

--- src/common/JDBCTemp.java
+++ src/common/JDBCTemp.java
@@ -1,95 +1,86 @@
 package common;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-public class JDBCTemp {
-	
+public interface JDBCTemp {
 	public static Connection getConnection() {
 		Connection conn = null;
-		Properties p = new Properties();
+		Properties prop = new Properties();
+
 		try {
-//			Class.forName("orcle.jdbc.driver.OracleDriver");
-//			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","student","student");
-			String cp = JDBCTemp.class.getResource("./").getPath();
-			p.load(new FileReader(cp + "jdbc.properties"));
-			
-			String driver = p.getProperty("driver");
-			String url = p.getProperty("url");
-			String user = p.getProperty("user");
-			String passwd = p.getProperty("passwd");
+			String currentPath = JDBCTemp.class.getResource("./").getPath();
+			prop.load(new FileReader(currentPath + "jdbc.properties"));
+
+			String driver = prop.getProperty("driver");
+			String url = prop.getProperty("url");
+			String user = prop.getProperty("user");
+			String passwd = prop.getProperty("passwd");
+
 			Class.forName(driver);
-			conn = DriverManager.getConnection(url,user,passwd);
+
+			conn = DriverManager.getConnection(url, user, passwd);
 			conn.setAutoCommit(false);
-		} catch (FileNotFoundException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (IOException e) {
+		}
+		return conn;
+	}
+
+	public static void close(Statement stmt) {
+		try {
+			if (stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
+		}
+	}
+
+	public static void close(ResultSet rset) {
+		try {
+			if (rset!= null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
+		}
+	
+	}
+
+	public static void close(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+	public static void commit(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		return conn;
 	}
-	
-
-	
-	public static void close(Connection conn) {
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void close(Statement conn) {
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void close(PreparedStatement conn) {
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void close(ResultSet conn) {
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
 	public static void rollback(Connection conn) {
 		try {
-			conn.rollback();
-		} catch (SQLException e) {
+			if (conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
-	
-	public static void commit(Connection conn) {
-		try {
-			conn.commit();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+
 }
