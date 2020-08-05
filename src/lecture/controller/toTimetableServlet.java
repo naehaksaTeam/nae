996e9e2f77a307a5904d01686d3bca5ca6bbd4ca
Jay13Jeong <63899204+Jay13Jeong@users.noreply.github.com>
2020-08-05 17:56:52 +0900

    시간표조회 만들기 시작~

--- src/lecture/controller/toTimetableServlet.java
+++ src/lecture/controller/toTimetableServlet.java
@@ -1,11 +1,16 @@
 package lecture.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import lecture.model.service.LectureService;
+import lecture.model.vo.TimeTable;
 
 /**
  * Servlet implementation class toTimetableServlet
@@ -26,8 +31,9 @@ public class toTimetableServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ArrayList<TimeTable> list = new LectureService().selectTimeTable();
+		
+		
 	}
 
 	/**
