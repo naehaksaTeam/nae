59abc2c9e3d2fa0ebbc526d4e2426cd1da04da8a
Jay13Jeong <63899204+Jay13Jeong@users.noreply.github.com>
2020-08-06 14:24:18 +0900

    member객체 생성

---
+++ src/student/model/vo/Member.java
@@ -0,0 +1,14 @@
+package student.model.vo;
+
+public class Member implements java.io.Serializable {
+	private static final long serialVersionUID = 77463L;
+	   private String id;
+	   private String name;
+	   private String ssn;
+	   private String address;
+	   private String phone;
+	   private String gender;
+	   private String email;
+	   private String treasure;
+	   private String password;
+}
