2dcd503849d948320852e33cf887307557718ae3
suugit <69067257+suugit@users.noreply.github.com>
2020-08-07 01:02:32 +0900

    jsp 추가
    123

--- src/attendance/controller/AtndnEditViewServlet.java
+++ src/attendance/controller/AtndnEditViewServlet.java
@@ -1,8 +1,6 @@
 package attendance.controller;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,45 +9,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import attendance.model.service.AtndnService;
-import attendance.model.vo.Atndn;
-
 /**
- * Servlet implementation class MyLctrServlet
+ * Servlet implementation class AtndnEditListServlet
  */
-@WebServlet("/mylctr")
-public class MyLctrServlet extends HttpServlet {
-	private static final long serialVersionUID = 417L;
+@WebServlet("/atnedit")
+public class AtndnEditViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MyLctrServlet() {
+    public AtndnEditViewServlet() {
         super();
-       
+        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String studentid = request.getParameter("studentid");
-		
-		ArrayList<Atndn> list = new AtndnService().selectMyLctr(studentid);
 		RequestDispatcher view = null;
-		
-		if(list.size() > 0) {
-			view = request.getRequestDispatcher("/views/attendance/myLctrPage.jsp");
-			request.setAttribute("list", list);
-			view.forward(request, response);
-		}else {
-			view = request.getRequestDispatcher("views/common/error.jsp");
-			request.setAttribute("message", "나의 강의목록 조회 실패");
-			view.forward(request, response);
-		}
-		
-		
+		view = request.getRequestDispatcher("views/attendance/atndnEdit.jsp");
 	}
 
 	/**
