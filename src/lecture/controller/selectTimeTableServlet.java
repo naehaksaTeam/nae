a53b1285968d746d990d860bf1167c4e98a01e93
Jay13Jeong <63899204+Jay13Jeong@users.noreply.github.com>
2020-08-07 15:47:19 +0900

    ccc

--- src/lecture/controller/selectTimeTableServlet.java
+++ src/lecture/controller/selectTimeTableServlet.java
@@ -36,22 +36,22 @@ public class selectTimeTableServlet extends HttpServlet {
 		String studentid = request.getParameter("who");
 		LectureService ls = new LectureService();
 		TimeTable list9 = new LectureService().selecTimeTable(studentid,"9");
-//		TimeTable list10 = new LectureService().selecTimeTable(studentid,"10");
-//		TimeTable list11 = new LectureService().selecTimeTable(studentid,"11");
-//		TimeTable list12 = new LectureService().selecTimeTable(studentid,"12");
-//		TimeTable list13 = new LectureService().selecTimeTable(studentid,"13");
-//		TimeTable list14 = new LectureService().selecTimeTable(studentid,"14");
-//		TimeTable list15 = new LectureService().selecTimeTable(studentid,"15");
+		TimeTable list10 = new LectureService().selecTimeTable(studentid,"10");
+		TimeTable list11 = new LectureService().selecTimeTable(studentid,"11");
+		TimeTable list12 = new LectureService().selecTimeTable(studentid,"12");
+		TimeTable list13 = new LectureService().selecTimeTable(studentid,"13");
+		TimeTable list14 = new LectureService().selecTimeTable(studentid,"14");
+		TimeTable list15 = new LectureService().selecTimeTable(studentid,"15");
 		System.out.println("sss");
 		RequestDispatcher view = null;
-		view = request.getRequestDispatcher("/views/lecture/강의계획서.jsp");
+		view = request.getRequestDispatcher("/views/lecture/시간표조회.jsp");
 		if(list9 != null)request.setAttribute("list9", list9);
-//		if(list10 != null)request.setAttribute("list10", list10);
-//		if(list11 != null)request.setAttribute("list11", list11);
-//		if(list12 != null)request.setAttribute("list12", list12);
-//		if(list13 != null)request.setAttribute("list13", list13);
-//		if(list14 != null)request.setAttribute("list14", list14);
-//		if(list15 != null)request.setAttribute("list15", list15);
+		if(list10 != null)request.setAttribute("list10", list10);
+		if(list11 != null)request.setAttribute("list11", list11);
+		if(list12 != null)request.setAttribute("list12", list12);
+		if(list13 != null)request.setAttribute("list13", list13);
+		if(list14 != null)request.setAttribute("list14", list14);
+		if(list15 != null)request.setAttribute("list15", list15);
 		view.forward(request, response);
 		
 	}
