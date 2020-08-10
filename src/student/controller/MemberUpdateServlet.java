9f3341acbf26ae67d8bc28fd7fb76841c8dfaba5
hckim21c11 <69066658+hckim21c11@users.noreply.github.com>
2020-08-10 16:31:28 +0900

    8월 10일 4시 31분

--- src/student/controller/MemberUpdateServlet.java
+++ src/student/controller/MemberUpdateServlet.java
@@ -19,64 +19,67 @@ import student.model.vo.Member;
 @WebServlet("/mupdate")
 public class MemberUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MemberUpdateServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-				//1.
-				request.setCharacterEncoding("utf-8");
-				
-				//2.
-				Member member = new Member();
-				
-				
-				member.setId(request.getParameter("id"));
-				member.setName(request.getParameter("name"));
-				member.setSsn(request.getParameter("ssn"));
-				member.setAddress(request.getParameter("address"));
-				member.setPhone(request.getParameter("phone"));
-				member.setGender(request.getParameter("gender"));
-				member.setEmail(request.getParameter("email"));
-				member.setTreasure(request.getParameter("treasure"));
-				member.setPassword(request.getParameter("password"));
-				member.setTreasure(request.getParameter("treasure"));
-				member.setAdminhiredate(Date.valueOf(request.getParameter("adminhiredate")));
-				
-			
-			
-			
-				member.setCategoryname(request.getParameter("categoryname"));
-			
-			
-				
-				//3.
-				int result = new MemberService().updateMember(member);
-				
-				//4.
-				if(result > 0) {  //수정 성공시
-					//서블릿을 실행해서, 내 정보보기 페이지를 내보냄
-					response.sendRedirect("/beet/");
-					}else {  //수정 실패시
-					RequestDispatcher view = request.getRequestDispatcher("views/common/error.jsp");
-					request.setAttribute("message", member.getId() + " 회원의 정보 수정 실패.");
-					view.forward(request, response);
-				}
+	public MemberUpdateServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 1.
+		request.setCharacterEncoding("utf-8");
+
+		// 2.
+		Member member = new Member();
+
+		member.setId(request.getParameter("id"));
+		member.setName(request.getParameter("name"));
+		member.setSsn(request.getParameter("ssn"));
+		member.setAddress(request.getParameter("address"));
+		member.setPhone(request.getParameter("phone"));
+		member.setGender(request.getParameter("gender"));
+		member.setEmail(request.getParameter("email"));
+		member.setTreasure(request.getParameter("treasure"));
+		member.setPassword(request.getParameter("password"));
+		member.setTreasure(request.getParameter("treasure"));
+		member.setAdminhiredate(Date.valueOf(request.getParameter("adminhiredate")));
+		member.setPassword(request.getParameter("password"));
+		member.setEntrancedate(Date.valueOf(request.getParameter("entrancedate")));
+		member.setAbsencewhether(request.getParameter("absencewhether"));
+		member.setAbsencecount(Integer.parseInt(request.getParameter("absenececount")));
+		member.setSsname(request.getParameter("ssname"));
+		member.setCategoryname(request.getParameter("categoryname"));
+		member.setMajorno(request.getParameter("majorno"));
+
+		// 3.
+		int result = new MemberService().updateMember(member);
+
+		// 4.
+		if (result > 0) { // 수정 성공시
+			// 서블릿을 실행해서, 로그인페이지 내보냄
+			response.sendRedirect("/beet/web/index.jsp");
+		} else { // 수정 실패시
+			RequestDispatcher view = request.getRequestDispatcher("views/common/error.jsp");
+			request.setAttribute("message", member.getId() + " 회원의 정보 수정 실패.");
+			view.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
