4599f60b4f9075df2d9349d3245ce7c9bbf815e3
Jay13Jeong <63899204+Jay13Jeong@users.noreply.github.com>
2020-08-11 09:26:37 +0900

    merge test

---
+++ src/student/controller/FindIdServlet.java
@@ -0,0 +1,71 @@
+package student.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import student.model.dao.MemberDao;
+import student.model.service.MemberService;
+import student.model.vo.Member;
+
+/**
+ * Servlet implementation class FindIdServlet
+ */
+@WebServlet("/findid")
+public class FindIdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FindIdServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 아이디 찾기 컨트롤러
+
+		// 1.
+		request.setCharacterEncoding("utf-8");
+
+		// 2.
+		Member member = new Member();
+		member.setName(request.getParameter("name"));
+		member.setTreasure(request.getParameter("treasure"));
+
+		// 3.
+		String result  = new MemberService().FindIdMember(String name, String treasure);
+
+		// 4.
+		if (result > 0) {
+
+			response.sendRedirect("beet/views/student/findId.jsp");
+		} else {
+			RequestDispatcher view = request.getRequestDispatcher("views/common/error.jsp");
+			request.setAttribute("message", "아이디찾기실패.");
+			view.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
