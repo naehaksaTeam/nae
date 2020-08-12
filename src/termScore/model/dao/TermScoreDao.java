8d48c1ecd824aa1226ae1ebb8436c67648283925
Jay13Jeong <63899204+Jay13Jeong@users.noreply.github.com>
2020-08-12 12:34:46 +0900

    복구완료
    규림씨 작업물이 로그인 에러 만듬

--- src/termScore/model/dao/TermScoreDao.java
+++ src/termScore/model/dao/TermScoreDao.java
@@ -1,6 +1,6 @@
 package termScore.model.dao;
 
-import static common.JDBCTemp.*;
+import static common.JDBCTemp.close;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,77 +11,6 @@ import java.util.ArrayList;
 import termScore.model.vo.TermScore;
 public class TermScoreDao {
 
-	//학생 전체학년 성적조회
-	public TermScoreDao() {
-	}
-
-	public TermScore selectTotalScore(Connection conn, String sid) {
-		Statement stmt = null;
-		ResultSet rset = null;
-		
-		TermScore tscore = new TermScore();
-		String query = "select sid, sum(termaplpoint) taplpoint, sum(termgetpoint) tgetpoint, avg(grdpoint) grdpoint, round(avg(tgpa),2) avgtgpa from TermScoreView where sid= '"+ sid +"' group by sid";
-		try {
-			
-			stmt =conn.createStatement();
-			rset = stmt.executeQuery(query);
-		
-			if(rset.next()) {
-				tscore.setSid(sid);
-				tscore.setTermaplpoint(rset.getInt("taplpoint"));
-				tscore.setTermgetpoint(rset.getInt("tgetpoint"));
-				tscore.setGrdpoint(rset.getInt("grdpoint"));
-				tscore.setTgpa(rset.getDouble("avgtgpa"));	
-	
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			close(rset);
-			close(stmt);
-		}
-		return tscore;
-	}
-
-	//학생 학기별 성적조회
-	public ArrayList<TermScore> selectTermScore(Connection conn, String sid) {
-		PreparedStatement pstmt = null;
-		ResultSet rset = null;
-		
-		ArrayList<TermScore> list = new ArrayList<TermScore>();
-		String query = "select sid, semester, termaplpoint, termgetpoint, grdpoint, tgpa from TermScoreView where sid= ?";
-
-		try {
-			
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, sid);
-			rset = pstmt.executeQuery();
-		
-			while(rset.next()) {
-				TermScore tscore = new TermScore();
-				
-				tscore.setSid(sid);
-				tscore.setSemester(rset.getString("semester"));
-				tscore.setTermaplpoint(rset.getInt("termaplpoint"));
-				tscore.setTermgetpoint(rset.getInt("termgetpoint"));
-				tscore.setGrdpoint(rset.getInt("grdpoint"));
-				tscore.setTgpa(rset.getDouble("tgpa"));	
-				
-				list.add(tscore);		
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			close(rset);
-			close(pstmt);
-		}
-		return list;
-	}
-	//관리자용 성적조회 ? 
-	
-//////////////////////////////////by kyu///////////
 	public ArrayList<TermScore> selectMain(Connection conn, String id) {
 		ArrayList<TermScore> list = new ArrayList<TermScore>();
 		PreparedStatement pstmt = null;
@@ -114,6 +43,21 @@ public class TermScoreDao {
 		}
 		System.out.println("dao"+ list);
 		return list;
-	}	
-/////////////////////////
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/termScore/model/service/TermScoreService.java
+++ src/termScore/model/service/TermScoreService.java
@@ -1,37 +1,22 @@
 package termScore.model.service;
 
+
+import static common.JDBCTemp.close;
 import java.sql.Connection;
 import java.util.ArrayList;
 
+import schedule.model.vo.Schedule;
 import termScore.model.dao.TermScoreDao;
 import termScore.model.vo.TermScore;
 import static common.JDBCTemp.*;
 
-public class TermScoreService {
-	TermScoreDao tdao = new TermScoreDao();
-	
-	public TermScoreService(){}
-	
-	public TermScore selectTotalScore(String sid) {
-		Connection conn = getConnection();
-		TermScore tscore = tdao.selectTotalScore(conn, sid);
-		close(conn);
-		return(tscore);
-	}
-	
-	public ArrayList<TermScore> selectTermScore(String sid){
-		Connection conn = getConnection();
-		ArrayList<TermScore> list = tdao.selectTermScore(conn, sid);
-		close(conn);
-		return(list);
-	}
-//////by kyu////
+
+
 	public ArrayList<TermScore> selectMain(String id) {
 		Connection conn = getConnection();
 		ArrayList<TermScore> list = tdao.selectMain(conn, id);
 		close(conn);
 		System.out.println("service"+ list);
 		return list;
-	}	
-////
+	}
 }
